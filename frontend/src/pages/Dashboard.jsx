b3cc2402bfc12965fd378188cfa421934ea9af6c
imransyed963 <syedimranimmu963@gmail.com>
2026-06-14 22:42:51 +0530

    connected frontend to depolyed backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-import {getAreas} from "../services/areaservice.js"
+import {getAreas} from "../services/areaService.js"
 
 import { getWaterSupply } from "../services/waterservice.js"
 
